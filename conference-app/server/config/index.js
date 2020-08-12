0f730cc1cb55d4db29f79dac47ac92002b307150
PascalUlor <pascalulor@yahoo.com>
2020-08-12 18:47:04 +0100

    integrate service with main app

--- conference-app/server/config/index.js
+++ conference-app/server/config/index.js
@@ -3,8 +3,9 @@ const path = require('path');
 module.exports = {
   development: {
     sitename: 'Roux Meetups [Development]',
+    serviceRegistryUrl: 'http://localhost:3000',
+    serviceVersionIdentifier: '1.x.x',
     data: {
-      speakers: path.join(__dirname, '../data/speakers.json'),
       feedback: path.join(__dirname, '../data/feedback.json'),
     },
   },
--- conference-app/server/services/Speakers.js
+++ conference-app/server/services/Speakers.js
@@ -1,77 +1,69 @@
-const fs = require('fs');
-const util = require('util');
+const axios = require('axios');
 
-const readFile = util.promisify(fs.readFile);
 
 class SpeakersService {
-  constructor(datafile) {
-    this.datafile = datafile;
+  constructor({ serviceRegistryUrl, serviceVersionIdentifier }) {
+    this.serviceRegistryUrl = serviceRegistryUrl;
+    this.serviceVersionIdentifier = serviceVersionIdentifier;
   }
 
   async getNames() {
-    const data = await this.getData();
-
-    return data.map(speaker => ({
-      name: speaker.name,
-      shortname: speaker.shortname,
-    }));
+    const { ip, port } = await this.getService('speakers-service');
+    return this.callService({
+      method: 'get',
+      url: `http://${ip}:${port}/names`
+    });
   }
 
   async getListShort() {
-    const data = await this.getData();
-    return data.map(speaker => ({
-      name: speaker.name,
-      shortname: speaker.shortname,
-      title: speaker.title,
-    }));
+    const { ip, port } = await this.getService('speakers-service');
+    return this.callService({
+      method: 'get',
+      url: `http://${ip}:${port}/list-short`
+    });
   }
 
   async getList() {
-    const data = await this.getData();
-    return data.map(speaker => ({
-      name: speaker.name,
-      shortname: speaker.shortname,
-      title: speaker.title,
-      summary: speaker.summary,
-    }));
+    const { ip, port } = await this.getService('speakers-service');
+    return this.callService({
+      method: 'get',
+      url: `http://${ip}:${port}/list`
+    });
   }
 
   async getAllArtwork() {
-    const data = await this.getData();
-    const artwork = data.reduce((acc, elm) => {
-      if (elm.artwork) {
-        // eslint-disable-next-line no-param-reassign
-        acc = [...acc, ...elm.artwork];
-      }
-      return acc;
-    }, []);
-    return artwork;
+    const { ip, port } = await this.getService('speakers-service');
+    return this.callService({
+      method: 'get',
+      url: `http://${ip}:${port}/artwork`
+    });
   }
 
   async getSpeaker(shortname) {
-    const data = await this.getData();
-    const speaker = data.find(current => current.shortname === shortname);
-    if (!speaker) return null;
-    return {
-      title: speaker.title,
-      name: speaker.name,
-      shortname: speaker.shortname,
-      description: speaker.description,
-    };
+    const { ip, port } = await this.getService('speakers-service');
+    return this.callService({
+      method: 'get',
+      url: `http://${ip}:${port}/speaker/${shortname}`
+    });
   }
 
   async getArtworkForSpeaker(shortname) {
-    const data = await this.getData();
-    const speaker = data.find(current => current.shortname === shortname);
-    if (!speaker || !speaker.artwork) return null;
-    return speaker.artwork;
+    const { ip, port } = await this.getService('speakers-service');
+    return this.callService({
+      method: 'get',
+      url: `http://${ip}:${port}/artwork/${shortname}`
+    });
   }
 
-  async getData() {
-    const data = await readFile(this.datafile, 'utf8');
-    if (!data) return [];
-    return JSON.parse(data).speakers;
-  }
+  async callService(requestOptions) {
+    const response = await axios(requestOptions);
+    return response.data;
+  };
+
+  async getService(servicename) {
+    const response = await axios.get(`${this.serviceRegistryUrl}/find/${servicename}/${this.serviceVersionIdentifier}`);
+    return response.data;
+  };
 }
 
 module.exports = SpeakersService;
